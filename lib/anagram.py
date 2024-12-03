8ce6ce430db5719a6221ef64d23882e3a34cc19e
Basil Wabuke <babazwabuke4@gmail.com>
2024-12-04 02:35:27 +0300

    adding changes to anagram.py file

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+from collections import Counter
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.word_count = Counter(word)
+
+    def match(self, word_list):
+        return [word for word in word_list if Counter(word) == self.word_count and word != self.word]
+    
+    def __repr__(self):
+        return f"Anagram('{self.word}')"
+    
+    
